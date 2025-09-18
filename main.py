9c94875ca6ee69d7fa00aa11e1d6504a10b77d97
OCarrollM <m.ocarroll@lancaster.ac.uk>
2025-09-18 20:42:50 +0100

    Changed port

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def get_workout():
     return jsonify(workouts), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
