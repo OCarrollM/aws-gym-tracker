754b8e1de87e8b60ce3d8d0f353b9db364eb21f3
OCarrollM <m.ocarroll@lancaster.ac.uk>
2025-09-18 13:26:32 +0100

    Simple python web, local API to post workouts from terminal

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+workouts = []
+
+# Home
+@app.route('/')
+def home():
+    return "Welcome to the Gym Tracker"
+
+# Add workout
+@app.route('/workout', methods=['POST'])
+def add_workout():
+    data = request.get_json()
+    
+    if not data or 'exercise' not in data or 'sets' not in data or 'reps' not in data:
+        return jsonify({"error:" "You have missed an exercise, set or rep"}), 400
+    
+    workout = {
+        "exercise": data['exercise'],
+        "sets": data['sets'],
+        "reps": data['reps']
+    }
+    
+    workouts.append(workout)
+    return jsonify({"message": "Workout added.", "workout": workout}), 201
+
+# Get workout
+@app.route('/workouts', methods=['GET'])
+def get_workout():
+    return jsonify(workouts), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
